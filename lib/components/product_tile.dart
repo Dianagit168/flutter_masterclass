f24a617f7e49bf5de045515a0cf2cd94c9306d3d
Dyna <dyna30432@gmail.com>
2024-11-08 19:47:46 +0700

    feat(intermidiate): completed minimal e-commerce app

--- lib/components/product_tile.dart
+++ lib/components/product_tile.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_maserclass/model/product_model.dart';
+import 'package:flutter_maserclass/model/shop_provider.dart';
+import 'package:provider/provider.dart';
 
 class ProductTile extends StatelessWidget {
   final ProductModel productModel;
@@ -7,6 +9,30 @@ class ProductTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    void addToCart() {
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            content: const Text('Add this item to your cart?'),
+            actions: [
+              MaterialButton(
+                onPressed: () => Navigator.pop(context),
+                child: const Text('Cancel'),
+              ),
+              MaterialButton(
+                onPressed: () {
+                  context.read<ProduProvider>().addItemToCart(productModel);
+                  Navigator.pop(context);
+                },
+                child: const Text('Yes'),
+              )
+            ],
+          );
+        },
+      );
+    }
+
     return Container(
       width: 300,
       padding: const EdgeInsets.all(25),
@@ -17,11 +43,47 @@ class ProductTile extends StatelessWidget {
             12,
           )),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          const Icon(Icons.favorite),
-          Text('${productModel.name}'),
-          Text('${productModel.name}'),
-          Text('${productModel.name}')
+          AspectRatio(
+            aspectRatio: 1,
+            child: Container(
+              padding: const EdgeInsets.all(25),
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.secondary,
+                borderRadius: BorderRadius.circular(
+                  12,
+                ),
+              ),
+              child: Image.asset(productModel.imageParth!),
+            ),
+          ),
+          const SizedBox(height: 25),
+          Text(
+            '${productModel.name}',
+            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+          ),
+          const SizedBox(height: 10),
+          Text('${productModel.description}',
+              style: TextStyle(
+                  color: Theme.of(context).colorScheme.inversePrimary)),
+          const Spacer(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('\$${productModel.price}'),
+              Container(
+                decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.secondary,
+                    borderRadius: BorderRadius.circular(12)),
+                child: IconButton(
+                  onPressed: addToCart,
+                  icon: const Icon(Icons.add),
+                ),
+              )
+            ],
+          )
         ],
       ),
     );
--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_maserclass/components/my_button.dart';
+import 'package:flutter_maserclass/model/product_model.dart';
+import 'package:flutter_maserclass/model/shop_provider.dart';
+import 'package:provider/provider.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final cartData = context.watch<ProduProvider>().userCart;
+    void removeFromCart(ProductModel productToRemove) {
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            content: const Text('Do you want to remome item cart?'),
+            actions: [
+              MaterialButton(
+                onPressed: () => Navigator.pop(context),
+                child: const Text('Cancel'),
+              ),
+              MaterialButton(
+                onPressed: () {
+                  context
+                      .read<ProduProvider>()
+                      .removeItemFromCart(productToRemove);
+                  Navigator.pop(context);
+                },
+                child: const Text('Yes'),
+              )
+            ],
+          );
+        },
+      );
+    }
+
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surface,
       appBar: AppBar(
@@ -12,6 +43,34 @@ class CartPage extends StatelessWidget {
         backgroundColor: Colors.transparent,
         title: const Text('My Cart'),
       ),
+      body: cartData.isEmpty
+          ? const Center(
+              child: Text('Your cart is empty...'),
+            )
+          : Column(
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: cartData.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        leading: Text('${cartData[index].name}'),
+                        title: Text('${cartData[index].price}'),
+                        trailing: IconButton(
+                            onPressed: () {
+                              removeFromCart(cartData[index]);
+                            },
+                            icon: const Icon(Icons.remove)),
+                      );
+                    },
+                  ),
+                ),
+                MyButton(
+                  onTap: () {},
+                  child: const Text("Pay Now!"),
+                )
+              ],
+            ),
     );
   }
 }
